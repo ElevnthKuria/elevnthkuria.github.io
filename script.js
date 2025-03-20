8783d26b4d6684f6e6d8d4c14e38616150f8199d
ElevnthKuria <kuria3kuria@outlook.com>
2025-03-20 21:23:55 +0300

    My Push

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+//index.js
+
+const hamburger = document.getElementById('hamburger'); 
+const menu = document.querySelector('.menu'); 
+
+hamburger.addEventListener('click', function () { 
+    const hamIcon = this.querySelector('.hamburger-icon'); 
+    const crossIcon = this.querySelector('.cross-icon'); 
+    if (hamIcon.style.display === "none") { 
+        hamIcon.style.display = "inline-block"
+        menu.style.display = "none"
+        crossIcon.style.display = "none"
+    } 
+    else { 
+        crossIcon.style.display = "inline-block"
+        hamIcon.style.display = "none"
+        menu.style.display = "block"
+    } 
+});
+
+
 class Pixel {
   constructor(canvas, context, x, y, color, speed, delay) {
     this.width = canvas.width;
@@ -272,3 +293,4 @@ class PixelCanvas extends HTMLElement {
 }
 
 PixelCanvas.register();
+
